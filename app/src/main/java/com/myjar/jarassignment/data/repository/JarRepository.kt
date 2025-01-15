46ee58b92e4add66eb42242757f00ee9dd56b9b4
tanmayshree <tanmay.25ts@gmail.com>
2025-01-15 12:41:06 +0530

    fixed the issue that the repository was not emitting the data

--- app/src/main/java/com/myjar/jarassignment/data/repository/JarRepository.kt
+++ app/src/main/java/com/myjar/jarassignment/data/repository/JarRepository.kt
@@ -13,6 +13,8 @@ class JarRepositoryImpl(
     private val apiService: ApiService
 ) : JarRepository {
     override suspend fun fetchResults(): Flow<List<ComputerItem>> = flow {
-        apiService.fetchResults()
+        apiService.fetchResults().let {
+            emit(it)
+        }
     }
 }
